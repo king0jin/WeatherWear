a5459e63dea99c14676317f6e862e2e205059a10
et0709 <et0709@naver.com>
2025-02-08 16:42:40 +0900

    Initial commit

---
+++ src/main/java/com/naver/et0709/weatherwear/WeatherWearApplication.java
@@ -0,0 +1,13 @@
+package com.naver.et0709.weatherwear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherWearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherWearApplication.class, args);
+	}
+
+}
